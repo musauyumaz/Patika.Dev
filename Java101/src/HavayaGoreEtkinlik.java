85523ab8b7d5b37829d534f0f7777d60423ff550
Musa UYUMAZ <78364995+musauyumaz@users.noreply.github.com>
2022-03-14 10:01:57 +0300

    Update HavayaGoreEtkinlik.java

--- Java101/src/HavayaGoreEtkinlik.java
+++ Java101/src/HavayaGoreEtkinlik.java
@@ -21,7 +21,7 @@ public class HavayaGoreEtkinlik {
                 (sicaklik >= 5 && sicaklik < 15) ?"Sinemaya Gidebilirsin" :
                         (sicaklik >= 15 && sicaklik < 25) ? "Pikniğe Gidebilirsin":
                                 (sicaklik >= 25) ? "Yüzmeye Gidebilirsin" : "Kafana Göre Takıl " +
-                                        "Derece 25 ten fazla Bence evde otur film izle kafana güneş geçmesin :)";
+                                        "Derece oldukça fazla Bence evde otur film izle kafana güneş geçmesin :)";
 
         System.out.println(derece);
 
