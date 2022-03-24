3de2ff444853c171c5473941b61649b44df9b428
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2022-03-24 15:45:26 +0300

    www.patika.dev

--- Java101/Sınıflar/src/BoxingGame/Main.java
+++ Java101/Sınıflar/src/BoxingGame/Main.java
@@ -2,8 +2,8 @@ package BoxingGame;
 
 public class Main {
     public static void main(String[] args) {
-        Fighter fighter1 = new Fighter("A",10,120,100,85);
-        Fighter fighter2 = new Fighter("B",20,85,85,0);
+        Fighter fighter1 = new Fighter("A",30,120,100,85);
+        Fighter fighter2 = new Fighter("B",30,120,100,85);
 
         Match match = new Match(fighter1,fighter2,85,100);
         match.run();
